cbcb3b1121c8dcc0d422aeece37f2dd2af991a98
Daniel Aprillio <danielaprillio02@gmail.com>
2021-10-09 16:57:15 +0700

    Testing to read firebase data

--- Smedy/app/src/main/java/com/example/smedy/view/FiretestActivity.java
+++ Smedy/app/src/main/java/com/example/smedy/view/FiretestActivity.java
@@ -2,15 +2,32 @@ package com.example.smedy.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.smedy.R;
 
 public class FiretestActivity extends AppCompatActivity {
 
+    private TextView testTextViewUsername, testTextViewEmail;
+    private Button testButtonSignOut;
+    private Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firetest);
+
+        initialize();
+
+    }
+
+    private void initialize() {
+        testTextViewUsername = findViewById(R.id.testTextViewUsername);
+        testTextViewEmail = findViewById(R.id.testTextViewEmail);
+        testButtonSignOut = findViewById(R.id.testButtonSignOut);
+
     }
 }
