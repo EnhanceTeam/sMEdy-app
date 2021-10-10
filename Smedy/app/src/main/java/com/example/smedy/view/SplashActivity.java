714dc5296ebe649e2cd33b007a06be3dd98791a7
MASTERLIM27 <andreaslim2002@gmail.com>
2021-10-10 23:02:00 +0700

    Bug Fix Activity Splash

--- Smedy/app/src/main/java/com/example/smedy/view/SplashActivity.java
+++ Smedy/app/src/main/java/com/example/smedy/view/SplashActivity.java
@@ -1,8 +1,10 @@
-package com.example.smedy;
+package com.example.smedy.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.smedy.R;
 
 public class SplashActivity extends AppCompatActivity {
 
