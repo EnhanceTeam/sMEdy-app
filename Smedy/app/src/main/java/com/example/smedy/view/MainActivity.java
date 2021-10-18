22c5a0b5619b7d703f453a02ce1b1d4b6330a76d
Timothy Kristanto <66807730+TimothyKristanto@users.noreply.github.com>
2021-10-18 20:11:46 +0700

    fixed app flow + bugs
    1. automatically go to the home fragment when user move to MainActivity
    2. go to login page after on boarding
    3. made a new xml for music adapter viewholder cardview
    4. made a top navigation bar in MeditationMusicFragment

--- Smedy/app/src/main/java/com/example/smedy/view/MainActivity.java
+++ Smedy/app/src/main/java/com/example/smedy/view/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        bottomNavbarMain.show(3, true);
+        bottomNavbarMain.show(1, true);
 
         bottomNavbarMain.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
             @Override
--- Smedy/app/src/main/java/com/example/smedy/view/MeditationMusicFragment.java
+++ Smedy/app/src/main/java/com/example/smedy/view/MeditationMusicFragment.java
@@ -1,16 +1,16 @@
 package com.example.smedy.view;
 
-import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
+import com.example.smedy.R;
 
-<<<<<<< HEAD
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MeditationMusicFragment#newInstance} factory method to
@@ -18,26 +18,47 @@ import androidx.fragment.app.Fragment;
  */
 public class MeditationMusicFragment extends Fragment {
     private Button btnMeditasiMeditationMusicFragment, btnMusikMeditationMusicFragment;
-    private View divMeditasiMeditationMusicFragment, divMusikMeditationMusicFragment;
-    private String menuBefore = "meditasi";
-||||||| 0269d3d
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MeditationMusicFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MeditationMusicFragment extends Fragment {
-=======
-import com.example.smedy.R;
->>>>>>> 35827062c7de6a85bb95a61e0689ff93b330462d
+    private View divMeditasiMeditationMusicFragment, divMusikMeditationMusicFragment, view;
+    private String menu = "meditasi";
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-public class MeditationMusicFragment extends Fragment {
-    private CardView meditationMusicCardView;
-    private Intent intent;
-    private View view;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public MeditationMusicFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MeditationMusicFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MeditationMusicFragment newInstance(String param1, String param2) {
+        MeditationMusicFragment fragment = new MeditationMusicFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
@@ -45,40 +66,27 @@ public class MeditationMusicFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_meditation_music, container, false);
 
-        initialize();
-        setListener();
-
-        return view;
-    }
-
-    private void setListener() {
-        meditationMusicCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intent = new Intent(getActivity(),MusicActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-<<<<<<< HEAD
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_meditation_music, container, false);
-
-        initVar(view);
+        initVar();
         setListener();
 
         // Inflate the layout for this fragment
         return view;
     }
 
+    private void initVar() {
+        btnMeditasiMeditationMusicFragment = view.findViewById(R.id.btnMeditasiMeditationMusicFragment);
+        btnMusikMeditationMusicFragment = view.findViewById(R.id.btnMusikMeditationMusicFragment);
+        divMusikMeditationMusicFragment = view.findViewById(R.id.divMusikMeditationMusicFragment);
+        divMeditasiMeditationMusicFragment = view.findViewById(R.id.divMeditasiMeditationMusicFragment);
+
+        loadFragment(new MeditationFragment());
+    }
+
     private void setListener() {
         btnMeditasiMeditationMusicFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(menuBefore.equals("music")){
+                if(menu.equals("musik")){
                     divMusikMeditationMusicFragment.setVisibility(View.INVISIBLE);
                     divMeditasiMeditationMusicFragment.setVisibility(View.VISIBLE);
                     divMeditasiMeditationMusicFragment.setTranslationX(300);
@@ -86,7 +94,7 @@ public class MeditationMusicFragment extends Fragment {
 
                     loadFragment(new MeditationFragment());
 
-                    menuBefore = "meditasi";
+                    menu = "meditasi";
                 }
             }
         });
@@ -94,7 +102,7 @@ public class MeditationMusicFragment extends Fragment {
         btnMusikMeditationMusicFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(menuBefore.equals("meditasi")){
+                if(menu.equals("meditasi")){
                     divMusikMeditationMusicFragment.setVisibility(View.VISIBLE);
                     divMeditasiMeditationMusicFragment.setVisibility(View.INVISIBLE);
                     divMusikMeditationMusicFragment.setTranslationX(-300);
@@ -102,29 +110,10 @@ public class MeditationMusicFragment extends Fragment {
 
                     loadFragment(new MusicFragment());
 
-                    menuBefore = "music";
+                    menu = "musik";
                 }
             }
         });
-    }
-
-    private void initVar(View view) {
-        btnMeditasiMeditationMusicFragment = view.findViewById(R.id.btnMeditasiMeditationMusicFragment);
-        btnMusikMeditationMusicFragment = view.findViewById(R.id.btnMusikMeditationMusicFragment);
-        divMeditasiMeditationMusicFragment = view.findViewById(R.id.divMeditasiMeditationMusicFragment);
-        divMusikMeditationMusicFragment = view.findViewById(R.id.divMusikMeditationMusicFragment);
-
-        loadFragment(new MeditationFragment());
-||||||| 0269d3d
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meditation_music, container, false);
-=======
-    private void initialize() {
-        meditationMusicCardView = view.findViewById(R.id.meditationMusicCardView);
->>>>>>> 35827062c7de6a85bb95a61e0689ff93b330462d
     }
 
     private void loadFragment(Fragment fragment){
@@ -133,6 +122,4 @@ public class MeditationMusicFragment extends Fragment {
                 .replace(R.id.consFragmentMeditationMusicFragment, fragment)
                 .commit();
     }
-
-
 }
